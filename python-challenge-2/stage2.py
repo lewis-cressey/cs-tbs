3cb1126cd9658bb16a2d386c2e76efa5ddf8f25d
lewis-cressey <63366182+lewis-cressey@users.noreply.github.com>
2022-02-01 10:00:44 +0100

    Add files via upload

--- python-challenge-2/stage2.py
+++ python-challenge-2/stage2.py
@@ -46,46 +46,30 @@ class Challenge:
         for cell in cells:
             cell.reset(cell.value in self.values)
 
-def gen_box():
-    values = []
-    center = 90
-    min = -4
-    max = 4
-    for i in range(min, max + 1):
-        values.append(center + 20 * i + min)
-        values.append(center + 20 * i + max)
-        values.append(center + 20 * min + i)
-        values.append(center + 20 * max + i)
-    return values
-
-def gen_primes(limit):
-    primes = []
-    for n in range(2, limit):
-        for prime in primes:
-            if n % prime == 0: break
-        else: primes.append(n)
-    return primes
-
 def seq(n, f):
     values = [n]
     while n < 200:
-        n = fn(n)
+        n = f(n)
         values.append(n)
     return values
 
+def spiral(x):
+    values = [x]
+    direction = [1, 20, -1, -20]
+    for length in range(10):
+        for i in range(length):
+            x += direction[0]
+            values.append(x)
+        direction.append(direction.pop(0))
+    return values
+
 challenges = [
-    Challenge(1, "Mark the number 10", [10]),
-    Challenge(2, "Mark every number in the horizontal line", range(45, 56)),
-    Challenge(3, "Mark every even number in the table", range(0, 200, 2)),
-    Challenge(4, "Mark every odd number in the table", range(1, 200, 2)),
-    Challenge(5, "Mark every number in the vertical line", range(28, 200, 20)),
-    Challenge(6, "Mark every multiple of 3 in the table", range(0, 200, 3)),    
-    Challenge(7, "Mark every multiple of 3 which is not also a multiple of 2", [ x for x in range(200) if x % 2 != 0 and x % 3 == 0 ]),
-    Challenge(8, "Mark every power of 2", [ 2**x for x in range(8) ]),
-    Challenge(9, "Mark every number in the geometric sequence", seq(1, lambda x: 2 * x + 1)),
-    Challenge(10, "Mark every number in the box shape", gen_box()),
-    Challenge(11, "Mark every prime_number", gen_primes(200)),
-    Challenge(12, "You have completed every challenge!", [584639]),
+    Challenge(1, "Mark every power of 2", [ 2**x for x in range(8) ]),
+    Challenge(2, "Mark the diagonal line", [ x for x in range(5, 200, 21) ]),
+    Challenge(3, "Mark the cross", [ x for x in range(5, 200, 21) ] + [ x for x in range(15, 200, 19) ]),
+    Challenge(4, "Mark every number in the geometric sequence", seq(1, lambda x: 2 * x + 1)),
+    Challenge(5, "Mark the spiral", spiral(90)),
+    Challenge(6, "You have completed every challenge!", [584639]),
 ]
 current_challenge = challenges.pop(0)
 
