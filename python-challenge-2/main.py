5e033220495375fd188ca31f3b03a68297d33fb3
lewis-cressey <63366182+lewis-cressey@users.noreply.github.com>
2022-02-04 13:47:57 +0100

    Add files via upload

--- python-challenge-2/main.py
+++ python-challenge-2/main.py
@@ -66,26 +66,40 @@ def gen_primes(limit):
         else: primes.append(n)
     return primes
 
+
 def seq(n, f):
     values = [n]
     while n < 200:
-        n = fn(n)
+        n = f(n)
         values.append(n)
     return values
 
+def spiral(x):
+    values = [x]
+    direction = [1, 20, -1, -20]
+    for length in range(10):
+        for i in range(length):
+            x += direction[0]
+            values.append(x)
+        direction.append(direction.pop(0))
+    return values
+    
 challenges = [
     Challenge(1, "Mark the number 10", [10]),
     Challenge(2, "Mark every number in the horizontal line", range(45, 56)),
     Challenge(3, "Mark every even number in the table", range(0, 200, 2)),
     Challenge(4, "Mark every odd number in the table", range(1, 200, 2)),
-    Challenge(5, "Mark every number in the vertical line", range(28, 200, 20)),
+    Challenge(5, "Mark every number in the vertical line", range(8, 200, 20)),
     Challenge(6, "Mark every multiple of 3 in the table", range(0, 200, 3)),    
-    Challenge(7, "Mark every multiple of 3 which is not also a multiple of 2", [ x for x in range(200) if x % 2 != 0 and x % 3 == 0 ]),
-    Challenge(8, "Mark every power of 2", [ 2**x for x in range(8) ]),
-    Challenge(9, "Mark every number in the geometric sequence", seq(1, lambda x: 2 * x + 1)),
-    Challenge(10, "Mark every number in the box shape", gen_box()),
-    Challenge(11, "Mark every prime_number", gen_primes(200)),
-    Challenge(12, "You have completed every challenge!", [584639]),
+    Challenge(7, "Mark the diagonal line", [ x for x in range(5, 200, 21) ]),
+    Challenge(8, "Mark every multiple of 3 which is not also a multiple of 9", [ x for x in range(0, 200, 3) if x % 9 != 0 ]),
+    Challenge(9, "Mark every power of 2", [ 2**x for x in range(8) ]),
+    Challenge(10, "Mark the cross", [ x for x in range(5, 200, 21) ] + [ x for x in range(15, 200, 19) ]),
+    Challenge(11, "Mark every number in the box shape", gen_box()),
+    Challenge(12, "Mark every prime_number", gen_primes(200)),
+    Challenge(13, "Mark every number in the geometric sequence", seq(1, lambda x: 2 * x + 1)),
+    Challenge(14, "Mark the spiral", spiral(90)),
+    Challenge(15, "You have completed every challenge!", [584639]),
 ]
 current_challenge = challenges.pop(0)
 
